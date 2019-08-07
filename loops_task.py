7a2e90e0d26a1fa0afd60355c387e68974b46e20
Mohammad Al Thunayan <malthunayan4@gmail.com>
2019-08-07 03:44:39 +0300

    Add total price to receipt

--- loops_task.py
+++ loops_task.py
@@ -15,6 +15,12 @@ receipt={'item name':item,'price':price,'quantity':quantity}
 print('-'*len('receipt')*2+'\n    Receipt\n'+'-'*len('receipt')*2)
 
 x=0
+total_price=[]
 while x<len(price):
+	total_price.append(price[x]*quantity[x])
 	print('%d %s '%(quantity[x],item[x])+str(price[x])+'KD')
 	x+=1
+print('-'*len('receipt')*2)
+
+
+print('Total: '+str(sum(total_price))+'KD')
