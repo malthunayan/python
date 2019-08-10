5f20cf04c82a309321b7930b73b6e3735a0f93b0
Mohammad Al Thunayan <malthunayan4@gmail.com>
2019-08-10 04:22:25 +0300

    Delete unnecessary x=0 and x+=1

--- loops_task.py
+++ loops_task.py
@@ -1,20 +1,18 @@
-item=[]
+items=[]
 while True:
 	name=input('Item (enter "done" when finished): ')
 	if name.lower()=='done':
 		break
 	price=float(input('Price: '))
 	quantity=int(input('Quantity: '))
-	item.append({'name':name,'price':price,'quantity':quantity})
+	items.append({'name':name,'price':price,'quantity':quantity})
 
 print('-'*len('receipt')*2+'\n   Receipt\n'+'-'*len('receipt')*2)
 
-x=0
 total_price=[]
-for x in range(len(item)):
-	total_price.append(item[x]['price']*item[x]['quantity'])
-	print('%d %s %.3fKD'%(item[x]['quantity'],item[x]['name'],item[x]['price']))
-	x+=1
+for x in range(len(items)):
+	total_price.append(items[x]['price']*items[x]['quantity'])
+	print('%d %s %.3fKD'%(items[x]['quantity'],items[x]['name'],items[x]['price']))
 
 print('-'*len('receipt')*2)
 print('Total: %.3fKD'%(sum(total_price)))
