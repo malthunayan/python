4d4e17aad867c6f2359e012c242a099d85529a5d
Mohammad Al Thunayan <malthunayan4@gmail.com>
2019-08-10 18:17:22 +0300

    Changed Manager class to inherit from Employee, converted the while loops to for loops, and kept the lists of the employees and managers out of the classes

--- classes_task.py
+++ classes_task.py
@@ -1,58 +1,36 @@
+from datetime import date
+
 class Employee:
-	employees=[]
 	def __init__(self,name,age,salary,employment_date,**kwargs):
 		self.name=name
 		self.age=age
 		self.salary=salary
 		self.employment_date=employment_date
-		Employee.employees.append(self.name)
-		Employee.employees.append(self.age)
-		Employee.employees.append(self.salary)
-		Employee.employees.append(self.get_working_years())
 
 		for attributes,values in kwargs.items():
 			setattr(self,attributes,values)
 
 	def get_working_years(self):
-		from datetime import date
 		return date.today().year-self.employment_date
 
-	# def __str__(self):
-	# 	x=0
-	# 	while x<len(Employee.employees):
-	# 		return 'Name: '+str(Employee.employees[x])+', Age: '+str(Employee.employees[x+1])+', Salary: '+str(Employee.employees[x+2])+', Working Years: '+str(Employee.employees[x+3])
-	# 		x+=4
-
-class Manager:
-	managers=[]
+class Manager(Employee):
 	def __init__(self,name,age,salary,employment_date,bonus_percentage,**kwargs):
-		self.name=name
-		self.age=age
-		self.salary=salary
-		self.employment_date=employment_date
+		super().__init__(name, age, salary, employment_date)
 		self.bonus_percentage=bonus_percentage
-		Manager.managers.append(self.name)
-		Manager.managers.append(self.age)
-		Manager.managers.append(self.salary)
-		Manager.managers.append(self.get_working_years())
-		Manager.managers.append(self.get_bonus())
 
 		for attributes,values in kwargs.items():
 			setattr(self,attributes,values)
 
 	def get_working_years(self):
-		from datetime import date
 		return date.today().year-self.employment_date
 
 	def get_bonus(self):
 		return self.bonus_percentage*self.salary
 
-	# def __str__(self):
-	# 	return 'Name: '+self.name+', Age: '+str(self.age)+', Salary: '+str(self.salary)+', Working Years: '+str(self.get_working_years())+', Bonus: '+str(self.get_bonus())
-
-
 print('Welcome to HR Pro 2019')
 dashes='-'*50
+employee_list=[]
+manager_list=[]
 
 while True:
 	print('Choose an action to do:')
@@ -66,26 +44,24 @@ while True:
 	else:
 		print(dashes)
 		if option==1:
-			if len(Employee.employees)==0:
+			if len(employee_list)==0:
 				print('No employees have been added to the list yet.')
-			x=0
-			while x<len(Employee.employees):
-				print('Name: '+Employee.employees[x]+', Age: '+str(Employee.employees[x+1])+', Salary: '+str(Employee.employees[x+2])+', Working Years: '+str(Employee.employees[x+3]))
-				x+=4
+			for employee in employee_list:
+				print('Name: '+employee.name+', Age: '+str(employee.age)+', Salary: '+str(employee.salary)+', Working Years: '+str(employee.get_working_years()))
 			print(dashes)
 		elif option==2:
-			if len(Manager.managers)==0:
+			if len(manager_list)==0:
 				print('No managers have been added to the list yet.')
-			x=0
-			while x<len(Manager.managers):
-				print('Name: '+Manager.managers[x]+', Age: '+str(Manager.managers[x+1])+', Salary: '+str(Manager.managers[x+2])+', Working Years: '+str(Manager.managers[x+3])+', Bonus: '+str(Manager.managers[x+4]))
-				x+=5
+			for manager in manager_list:
+				print('Name: '+manager.name+', Age: '+str(manager.age)+', Salary: '+str(manager.salary)+', Working Years: '+str(manager.get_working_years())+', Bonus: '+str(manager.get_bonus()))
 			print(dashes)
 		elif option==3:
-			emp=Employee(input('Name: ').title(),input('Age: '),float(input('Salary: ')),int(input('Employment date: ')))
+			employee=Employee(input('Name: ').title(),input('Age: '),float(input('Salary: ')),int(input('Employment date: ')))
+			employee_list.append(employee)
 			print('\nEmployee added successfully!\n')
 		elif option==4:
-			man=Manager(input('Name: ').title(),input('Age: '),float(input('Salary: ')),int(input('Employment date: ')),float(input('Bonus Percentage: ')))
+			manager=Manager(input('Name: ').title(),input('Age: '),float(input('Salary: ')),int(input('Employment date: ')),float(input('Bonus Percentage: ')))
+			manager_list.append(manager)
 			print('\nManager added successfully!\n')
 		elif option==5:
 			break
